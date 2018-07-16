e25d1c3b4befc738f3fee76840850729266d806e
Liz <lizlie.holleza@awsys-i.com>
2018-07-16 15:11:41 +0800

    Add option adapter and implements the fragment pager method

---
+++ app/src/main/java/com/example/lizlieholleza/musicme/OptionAdapter.java
@@ -0,0 +1,21 @@
+package com.example.lizlieholleza.musicme;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class OptionAdapter extends FragmentPagerAdapter {
+    public OptionAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
